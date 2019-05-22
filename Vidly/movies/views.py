25e32cfa4ffa9707659588c8fdd05f6e685ea5b5
Kartug <karu46karthik@gmail.com>
2019-05-22 08:45:54 +0530

    A movie renting app
    Used Api and tried hosting it with heroku

---
+++ Vidly/movies/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+# Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    # # select * FROM movies_movie
+    # Movie.objects.filter(release_year=2000)
+    # # select * FROM movies_movie WHERE
+    # Movie.objects.get()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
